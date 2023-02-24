4ff4a0f999057f7cea5c6d5180117e84a9df1b0a
Daniel <danielverissimo1d@gmail.com>
2023-02-24 17:35:30 -0300

    Arquivos .py adicionados

---
+++ algoritimoUniasselvi.py
@@ -0,0 +1,7 @@
+
+
+#n1=int(input('digite um número '))
+#n2=int(input('digite o segundo número '))
+#soma = (n1 + n2)
+#print('soma= ', soma) 
+
---
+++ programaColega.py
@@ -0,0 +1,9 @@
+codigo = int(input("Digite o código do funcionário"))
+nome = input("Digite o nome do funcionário")
+salario = float(input("informe o salário"))
+ativo = True
+
+print(f'Codigo: {codigo}')
+print(f'Nome: {nome}')
+print(f'Salário: R${codigo:.2f}')
+print(f'Ativo: {ativo}')
